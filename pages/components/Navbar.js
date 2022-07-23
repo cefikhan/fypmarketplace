f459a507777aab639968b213400392dae1f4396b
cefi <saffikahn1@gmail.com>
2022-07-24 01:24:53 +0500

    commited

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -13,10 +13,25 @@ function Navbar(){
 
 
     const [click,setClick] = useState(false);
-    const [dropdown,setDropdown] = useState(true);
+    const [dropdown,setDropdown] = useState(false);
     const handleClick = () => setClick(!click);
     const closeMobileMenu=()=>setClick(false)
 
+    const onMouseEnter = ()=>{
+        if(window.innerWidth<960){
+            setDropdown(false)
+        }else{
+            setDropdown(true)
+        }
+    }
+
+        const onMouseLeave = ()=>{
+        if(window.innerWidth<960){
+            setDropdown(false)
+        }else{
+            setDropdown(false)
+        }
+    }
 
 
     return(<>
@@ -34,29 +49,28 @@ function Navbar(){
 
         <ul className={click ?`${styles.nav_menu} ${styles.active}`:`${styles.nav_menu}`}>
 
+               <li className={styles.nav_item}>
+                      <Link href='/' className={styles.nav_links}  onClick={closeMobileMenu}>
+                              Home            
+                    </Link>
+              </li>
+
+              <li className={styles.nav_item}   onMouseEnter={onMouseEnter}  onMouseLeave={onMouseLeave}>          
+                <Link href='/' className={styles.nav_links}  onClick={closeMobileMenu}>
+                <>  Services <FontAwesomeIcon icon={faCaretDown} />        
+                    </></Link>
+                {dropdown && <Dropdown/>}
+            </li>
+
+
         <li className={styles.nav_item}>
-            <Link href='/' className={styles.nav_link}  onClick={closeMobileMenu}>
-             Home            
-            </Link>
-        </li>
-
-
-        <li className={styles.nav_item}>          
-          <Link href='/' className={styles.nav_link}  onClick={closeMobileMenu}>
-           <>  Services <FontAwesomeIcon icon={faCaretDown} />        
-            </></Link>
-            {dropdown && <Dropdown/>}
-        </li>
-
-
-        <li className={styles.nav_item}>
-            <Link href='/' className={styles.nav_link}  onClick={closeMobileMenu}>
+            <Link href='/' className={styles.nav_links}  onClick={closeMobileMenu}>
              Contact Us            
             </Link>
         </li>
 
         <li className={styles.nav_item}>
-            <Link href='/' className={styles.nav_link}  onClick={closeMobileMenu}>
+            <Link href='/' className={styles.nav_links}  onClick={closeMobileMenu}>
              Sign Up        
             </Link>
         </li>
